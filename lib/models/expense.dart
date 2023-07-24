61025e70309ccf3504c05b2b8c9632af1073b680
e5e8 <iiibdr11@gmail.com>
2023-07-24 09:57:36 +0300

    adding icons and intl package

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
-const uuid =  Uuid();
+const uuid = Uuid();
 
+final formatter = DateFormat.yMd();
 
-enum Category{ food, travel, leisure , work}
+enum Category { food, travel, leisure, work }
+
+const categoryIcon = {
+  Category.food: Icons.lunch_dining,
+  Category.leisure: Icons.movie,
+  Category.travel: Icons.flight_takeoff,
+  Category.work : Icons.work
+};
 
 class Expense {
   Expense({
@@ -13,10 +23,14 @@ class Expense {
     required this.category,
   }) : id = uuid.v4();
 
-  
   final String title;
   final double amount;
   final String id;
   final DateTime date;
   final Category category;
+
+
+  String get formattedDate{
+    return formatter.format(date) ;
+  }
 }
