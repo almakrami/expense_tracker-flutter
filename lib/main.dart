24e302481c216b7cc107a71eae0f9d290fde169c
e5e8 <iiibdr11@gmail.com>
2023-07-30 14:55:47 +0300

    start theaming the application

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/Widgets/expenses.dart';
 
-void main() {
-  runApp(MaterialApp
-  (
-    theme: ThemeData(useMaterial3: true),
-    home:  const Expenses(),
-  ),);
-}
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 170, 0, 255),
+);
 
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+              
+          )),
+      home: const Expenses(),
+    ),
+  );
+}
